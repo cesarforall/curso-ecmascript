e6f3f62b70a1a20a91c20f9651da7d4bb295da0a
César Almeida <cesaralmeida89@gmail.com>
2023-01-26 20:46:55 +0100

    :note: ad es8 object entries and values

---
+++ src/es8/01-object-entries.js
@@ -0,0 +1,9 @@
+// Object.entries(objeto) transforma un objeto a array
+
+const countries = {
+	mx: 'México',
+	co: 'Colombia',
+	sp: 'Spain',
+};
+
+console.log(Object.entries(countries));
---
+++ src/es8/02-object-values.js
@@ -0,0 +1,9 @@
+// Object.values(objeto) devuelve los value en un array
+
+const countries = {
+	mx: 'México',
+	co: 'Colombia',
+	sp: 'Spain',
+};
+
+console.log(Object.values(countries));
