a0f648f2b64b618865f7fd275121d83e67c9fcba
César Almeida <cesaralmeida89@gmail.com>
2023-01-27 23:03:08 +0100

    :pencil: add es8 trailling commas

---
+++ src/es8/04-trailing-commas.js
@@ -0,0 +1,8 @@
+// Trailling comma son posiciones en un array
+
+const array = [24, 34, 25, 24];
+const arrayTC = [24, 34, 25, 24, , , , , 45];
+console.log(array);
+console.log(array.length);
+console.log(arrayTC);
+console.log(arrayTC.length);
