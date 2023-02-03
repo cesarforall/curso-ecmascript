549e6eae1e8ab95432071c0b30f0922d8d1c5488
César Almeida <cesaralmeida89@gmail.com>
2023-02-03 19:47:18 +0100

    :memo: add es12 numeric-separators

---
+++ src/es12/01-numeric-separators.js
@@ -0,0 +1,4 @@
+// Numeric separators son una forma visual de separar los valores de objetos primitivos de tipo número sin afectar a su valor de retorno.
+// Se usa poniendo guiones bajos para hacer las separaciones al inicializar con un valor.
+const value = 100_000_000_000;
+console.log(value);
