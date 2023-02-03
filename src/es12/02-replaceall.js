686ab094ebc5754a1653dada479a62e002e94296
César Almeida <cesaralmeida89@gmail.com>
2023-02-03 19:51:53 +0100

    :pencil: add es12 replace all

---
+++ src/es12/02-replaceall.js
@@ -0,0 +1,7 @@
+// .replace('caracteres', 'nuevos caracteres') es un método de los objetos primitivos de tipo string que realiza una búsqueda y reemplaza los  caracteres encontrados por unos nuevos.
+
+const string = 'Javascript es un maravilloso lenguaje de programación';
+
+const replacedString = string.replace('Javascript', 'Typescript');
+
+console.log(replacedString);
