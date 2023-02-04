a258508f4aea0d45abd37e078395bc63f640819c
César Almeida <cesaralmeida89@gmail.com>
2023-02-04 09:48:15 +0100

    :pen: add es13 at

---
+++ src/es13/01-at.js
@@ -0,0 +1,7 @@
+// El método .at(number) del objeto no primitivo Array devuelve el valor del índice (posición) pasado como argumento.
+
+const array = ['one', 'two', 'three', 'four', 'five', 'six'];
+
+console.log(array[array.length - 1]);
+
+console.log(array.at(-1));
